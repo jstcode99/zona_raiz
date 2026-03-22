260a7c57fac1225a261fbc8e83ceeb3a3c18f398
jstcode99 <jstorres0211@gmail.com>
2026-03-21 23:01:12 -0500

    feat(nav): update logo, main nav and route hook

--- features/navigation/logo.tsx
+++ features/navigation/logo.tsx
@@ -1,14 +1,16 @@
-import LogoSvg from '@/assets/svg/logo'
-import { cn } from '@/lib/utils'
-import i18next from 'i18next'
+import LogoSvg from "@/assets/svg/logo";
+import { cn } from "@/lib/utils";
+import { useTranslation } from "react-i18next";
 
 const Logo = ({ className }: { className?: string }) => {
-  return (
-    <div className={cn('flex items-center gap-2.5', className)}>
-      <LogoSvg className='size-8.5' />
-      <span className='text-xl font-semibold'>{i18next.t('app.name')}</span>
-    </div>
-  )
-}
+  const { t } = useTranslation("landing");
 
-export default Logo
+  return (
+    <div className={cn("flex items-center gap-2.5", className)}>
+      <LogoSvg className="size-8.5" />
+      <span className="text-xl font-semibold">{t("nav.brand")}</span>
+    </div>
+  );
+};
+
+export default Logo;
