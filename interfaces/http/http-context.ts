ffa186dec94bbffc9bb96a194ece19756f3622d1
jstcode99 <jstorres0211@gmail.com>
2026-03-23 08:36:29 -0500

    chore: clear import

--- interfaces/http/http-context.ts
+++ interfaces/http/http-context.ts
@@ -1,15 +1,12 @@
-import { cookies as nextCookies } from "next/headers"
-import { NextRequest, NextResponse } from "next/server"
+import { cookies as nextCookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-import type {
-  RequestCookies,
-  ResponseCookies,
-} from "next/dist/compiled/@edge-runtime/cookies"
+import type { RequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
 
-export type ReadonlyCookies = Awaited<ReturnType<typeof nextCookies>>
+export type ReadonlyCookies = Awaited<ReturnType<typeof nextCookies>>;
 
 export interface CookieContext {
-  request?: NextRequest
-  response?: NextResponse
-  cookies?: ReadonlyCookies | RequestCookies
+  request?: NextRequest;
+  response?: NextResponse;
+  cookies?: ReadonlyCookies | RequestCookies;
 }
