23af2c49b89345b10cc464df28e00d1641d9ae82
jstcode99 <jstorres0211@gmail.com>
2026-03-26 08:37:20 -0500

    fix: add public and extencion on search_path pg

--- supabase/migrations/00000000000000_init_extensions.sql
+++ supabase/migrations/00000000000000_init_extensions.sql
@@ -1,2 +1,3 @@
 create extension if not exists "uuid-ossp" with schema extensions;
 create extension if not exists "pgcrypto" with schema extensions;
+alter database postgres set search_path = public, extensions;
